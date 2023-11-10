d2e1432d7528b7dc705ca81c3c6d3d55cf0043f1
Darpan <darpanneve3@gmail.com>
2023-11-10 13:31:44 +0530

    new changes

---
+++ chai_aur_code/1_basics/6_string.js
@@ -0,0 +1,9 @@
+const name='darpan'
+const surname=' Neve'
+console.log(`My name is ${name} and surname is ${surname}`)
+
+const newName = new String ("DARPAN")
+console.log(newName[0])
+console.log(name[0])
+console.log(newName.__proto__);
+console.log(name.__proto__);
