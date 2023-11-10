afbb42cfc01d84c51948795234444f59a08e840e
Darpan <darpanneve3@gmail.com>
2023-11-10 22:48:43 +0530

    num added

--- chai_aur_code/1_basics/6_string.js
+++ chai_aur_code/1_basics/6_string.js
@@ -2,8 +2,14 @@ const name='darpan'
 const surname=' Neve'
 console.log(`My name is ${name} and surname is ${surname}`)
 
-const newName = new String ("DARPAN")
+const newName = new String ("   DARPAN   ")
 console.log(newName[0])
 console.log(name[0])
 console.log(newName.__proto__);
-console.log(name.__proto__);
+console.log(newName.length);
+console.log(newName.charAt(2));
+ 
+console.log(newName)
+console.log(newName.trim())
+
+
---
+++ chai_aur_code/1_basics/7_num_maths.js
@@ -0,0 +1,27 @@
+// const score=100;
+// console.log(score);
+// const score1=new Number(10);
+// console.log(score1);
+// console.log(score1.toString());
+// console.log(score1.toFixed(2));
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//-----------------------------Maths-----------------------------
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
