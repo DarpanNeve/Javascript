706cf275d44b391adcc8ec86a1d22694229ceec9
Darpan <darpanneve3@gmail.com>
2023-12-06 10:53:24 +0530

    minor changes

--- javascript_chai_aur_code/06_dom/02_1.js
+++ javascript_chai_aur_code/06_dom/02_1.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("button");
   const parent = document.querySelector(".parent");
   button.addEventListener("click", () => {
-    console.log(parent);
-    console.log(parent.children);
+    // console.log(parent);
+    // console.log(parent.children);
+    for (let i = 0; i < parent.children.length; i++) {
+      console.log(parent.children[i].innerHTML);
+      parent.children[i].style.color = "red";
+    }
+    console.log(parent.firstElementChild);
   });
 });
