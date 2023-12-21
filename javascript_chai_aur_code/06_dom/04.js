f54a371b342a108faecb298fc633afb6238d9cdf
Darpan <darpanneve3@gmail.com>
2023-12-21 12:46:07 +0530

    minor changes

--- javascript_chai_aur_code/06_dom/04.js
+++ javascript_chai_aur_code/06_dom/04.js
@@ -1,6 +1,6 @@
 function addLanguage(langName) {
   const li = document.createElement("li");
   li.innerHTML = `${langName}`;
-  document.querySelector(".language");
-  appendChild(li);
+  document.querySelector(".language").appendChild(li);
 }
+addLanguage("python");
